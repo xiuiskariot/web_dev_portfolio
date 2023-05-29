66b98b8a4e81b37f7232262105240f8f672b275a
Ksusha <xiuschv@yandex.ru>
2023-05-29 14:27:53 +0300

    add scroll

--- src/ui_comp/about_snippets/AboutSnippets.jsx
+++ src/ui_comp/about_snippets/AboutSnippets.jsx
@@ -1,15 +1,16 @@
 import ReactEmbedGist from "react-embed-gist";
-import s from "./AboutSnippets.module.css"
-import my_photo from "./my_photo.jpg"
-import details_i from "./details.svg"
-import { useState } from "react";
+import s from "./AboutSnippets.module.css";
+import my_photo from "./my_photo.jpg";
+import details_i from "./details.svg";
+import { useRef, useState, useEffect } from "react";
+import debounce from "lodash.debounce";
 
 export const AboutSnippets = () => {
-  const [details1, setDetails1] = useState("")
+  const [details1, setDetails1] = useState("");
   const [details2, setDetails2] = useState("");
 
   return (
-    <div className={s.container}>
+    <div className={s.container} >
       {/* eslint-disable-next-line */}
       <p className={s.about_snippet_title}>// Code snippet showcase:</p>
       <div className={s.about_snippets}>
@@ -31,7 +32,6 @@ export const AboutSnippets = () => {
           <ReactEmbedGist gist="xiuiskariot/300d3958bc4ccdd173c0053fd0ba387d" />
           <p className={details1 === "first" ? s.details_active : s.details}>
             fisrt experience of using my server
-
           </p>
         </div>
         <div className={s.about_snippet}>
@@ -60,9 +60,7 @@ export const AboutSnippets = () => {
 };
 
 // 57248d8b88879a6ba77410e144622c60
-        
-          /* <ReactEmbedGist gist="xiuiskariot/b38c0a172a4a12025ecc014cf65fde3b" /> */
-        
-        
-          /* <ReactEmbedGist gist="xiuiskariot/57248d8b88879a6ba77410e144622c60" /> */
-        
+
+/* <ReactEmbedGist gist="xiuiskariot/b38c0a172a4a12025ecc014cf65fde3b" /> */
+
+/* <ReactEmbedGist gist="xiuiskariot/57248d8b88879a6ba77410e144622c60" /> */
